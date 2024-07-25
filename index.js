06d665b91c8edde9d5b81f01c068069d1113e90c
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-25 18:47:59 -0500

    added shape constructor, tests and prompt from inquireor

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const inqur = require('inquirer');
+// const sh = require('./lib/shapes');
+// const sv = require('./lib/svg');
+
+inqur
+.prompt([
+{
+    type:"list",
+    name:"shape",
+    message: "What kind of shape would you like the logo to be?",
+    choices:['circle','square','triangle'],
+},
+{
+    type: "input",
+    name:"text",
+    message:" Enter text for the logo (Must not be more than 3 Characters)",
+},
+{
+    type:"input",
+    name:"shapeColor",
+    message:"please enter a color for the logo shape",
+
+},
+{
+    type:"input",
+    name:"txtColor",
+    message:"please enter a text color",
+}
+])
+.then( (answers)=>{
+console.log(answers);
+return answers;
+}
+
+);
+//===============================================================example file path for examples============================================================================
+// .then((answers)=>
+//     fs.writeFile('./examples/answers.txt',`${answers.shape}\n${answers.text} \n${answers.shapeColor}\n${answers.txtColor}`,(err)=> err? console.log('oops something went wrong'):console.log('success!'))
+// );
