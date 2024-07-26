f7ca6d8a71719c66810ae60b32a358cbe72ab682
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-07-26 17:39:55 -0500

    added logic to index.js and introduced vallidate block to check for proper user input

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inqur = require('inquirer');
-const {Shape,Circle,Triangle} = require('./lib/shape.js');
+const {Shape,Circle,Triangle,Square} = require('./lib/shape.js');
 
 
 
@@ -16,6 +16,13 @@ inqur
     type: "input",
     name:"text",
     message:" Enter text for the logo (Must not be more than 3 Characters)",
+    validate: (userInput) =>{
+        if (userInput.length > 3){
+            return('Logo Text cannot contain more than 3 Characters');
+        }else{
+            return true;
+        }
+    } 
 },
 {
     type:"input",
@@ -30,14 +37,18 @@ inqur
 }
 ])
 .then( (answers)=>{
-    if(answers.text.length>3){
-        throw new Error('Invalid input please make sure your input does not exceed 3 characters');
-    }
-    else if(answers.shape ==='circle'){
+   
+     if(answers.shape ==='circle'){
         const circ = new Circle(answers.shape,answers.shapeColor.toLowerCase(),answers.text.toUpperCase(),answers.txtColor.toLowerCase());
         const shapeToRender = circ.renderCircle();
         return shapeToRender;
-    } 
+    } else if(answers.shape ==='square'){
+        const sqr = new Square(answers.shape,answers.shapeColor.toLowerCase(),answers.text.toUpperCase(),answers.txtColor.toLowerCase());
+        const shapeToRender = sqr.renderSquare();
+        return shapeToRender;
+
+
+    }
     else{
         const tri = new Triangle(answers.shape,answers.shapeColor.toLowerCase(),answers.text.toUpperCase(),answers.txtColor.toLowerCase());
         const shapeToRender= tri.renderTriangle();
